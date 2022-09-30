ae9735ded3b32561e6e7f09ad9f59706300fe2a2
Greg <garnold.805@gmail.com>
2022-09-30 18:48:19 -0400

    adds some formatting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img />
+        <p>Add some meat & potatoes</p>
         <p>
-          Add some meat & potatoes
+          <a
+            className="App-link"
+            href="https://v-for-vehicle.gregarnold.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            V-for-Vehicle
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/garnold805/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Linkedin
-        </a>
+        <p>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/garnold805/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+          </a>
+        </p>
       </header>
     </div>
   );
